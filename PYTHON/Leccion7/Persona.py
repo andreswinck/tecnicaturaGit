d15e9c11b7e239fae2328308f61a4cdc7c0b6e92
Andres Winckler <andreswinckler@gmail.com>
2022-11-02 11:14:52 -0300

    Commit de leccion 8 python

--- PYTHON/Leccion7/Persona.py
+++ PYTHON/Leccion7/Persona.py
@@ -1,16 +1,40 @@
+from errno import EADDRNOTAVAIL
+
+
 class Persona:
     def __init__(self, nombre, edad):
         self._nombre = nombre
         self._edad = edad
 
+    @property
+    def nombre(self):
+        return self._nombre
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+    
+    @property
+    def edad(self):
+        return self._edad
+    @edad.setter
+    def edad(self, edad):
+        self._edad = edad 
+
     def __str__(self): # Override = Sobreescribir
-        return f"Persona: [ Nombre: {self._nombre}, Edad: {self.edad}]"
+        return f"Persona: [ Nombre: {self._nombre}, Edad: {self._edad}]"
 
 
 class Empleado(Persona):  # Hereda de la clase Persona
     def __init__(self, nombre, edad, sueldo):
         super().__init__(nombre, edad)
-        self.sueldo = sueldo
+        self._sueldo = sueldo
+
+    @property
+    def sueldo(self):
+        return self._sueldo
+    @sueldo.setter
+    def sueldo(self, sueldo):
+        self._sueldo = sueldo
 
 
 empleado1 = Empleado("Ariel", 40, 75000)
@@ -22,3 +46,17 @@ print(empleado1.sueldo)
 # tarea: Encapsular los atributos y agregar los metodos getters and setters
 # Crear otro objeto, pasar los datos para nomnbre, edad y sueldo
 # Mostrar estos datos, luego modificar y mostrar nuevamente
+
+empleado2 = Empleado("Liliana",38, 70000)
+print(empleado2.nombre)
+print(empleado2.edad)
+print(empleado2.sueldo)
+
+empleado2.nombre = "Natalia"
+empleado2.edad = 35
+empleado2.sueldo = 75000
+
+print(empleado2.nombre)
+print(empleado2.edad)
+print(empleado2.sueldo)
+
--- PYTHON/Leccion8/Vehiculo.py
+++ PYTHON/Leccion8/Vehiculo.py
@@ -1,21 +1,61 @@
+"""
+    Definir una clase padre llamada Vehiculo y dos clases hijas llamadas
+    Auto y bicicleta, las cuales heredan de la clase padre Vehiculo. La clase
+    padre deber tener los siguientes atributos y metodos
+
+    Vehiculo(clase padre)
+    -Atributos (color, ruedas)
+    -Metodos (__init__(color,rueda) y __str__())
+
+    Auto(clase hija de vehiculo)
+    -Atributos(velocidad (km/hr))
+    -metodods(--init--(color, ruedas, velocidad) y __str__())
+
+    -Bicicleta(clase hija de vehiculo)
+    -Atributos(tipo(urbana/montaña/etc.)
+    -Metodos(__init__(color, ruedas, tipo) y __str__())
+
+    crear un objeto de cada clase
+
+    """
+
 class Vehiculo:
-"""
-Definir una clase padre llamada Vehiculo y dos clases hijas llamadas
-Auto y bicicleta, las cuales heredan de la clase padre Vehiculo. La clase
-padre deber tener los siguientes atributos y metodos
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
 
-Vehiculo(clase padre)
--Atributos (color, ruedas)
--Metodos (__init__(color,rueda) y __str__())
+    def __str__(self):
+        return "color: "+ self.color + " \nruedas: "+ str(self.ruedas)
 
-Auto(clase hija de vehiculo)
--Atributos(velocidad (km/hr))
--metodods(--init--(color, ruedas, velocidad) y __str__())
 
--Bicicleta(clase hija de vehiculo)
--Atributos(tipo(urbana/montaña/etc.)
--Metodos(__init__(color, ruedas, tipo) y __str__())
 
-crear un objeto de cada clase
 
-"""
+class Auto(Vehiculo):
+    def __init__(self, color, ruedas, velocidad):
+        super().__init__(color, ruedas)
+        self.velocidad = velocidad
+    
+    def __str__(self):
+        return super().__str__()+ "\nVelocidad(km/h): "+str(self.velocidad)
+
+        
+
+class Bicicleta(Vehiculo):
+    def __init__(self, color, ruedas, tipo):
+        super().__init__(color, ruedas)
+        self.tipo = tipo
+    
+    def __str__(self):
+        return super().__str__()+"\nTipo: "+ self.tipo
+
+
+# primer objeto clase padre ---> vehiculo
+vehiculo = Vehiculo("rojo",4)
+print("Objeto clase Vehiculo\n",vehiculo)
+#Segundo objeto , el primero de la clase Auto
+auto= Auto("Amarillo",6,140)
+print("\nobjeto clase Auto\n\n", auto)
+#Tercer objeto, primero de la clase Bicicleta
+bicicleta = Bicicleta("Negra", 2,"Todo Terreno")
+print("\nObjeto clase Bicicleta\n\n",bicicleta)
+
