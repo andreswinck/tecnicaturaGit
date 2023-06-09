93762bbbb626f4cdd26d061f9a0a21f8391f798f
Andres Winckler <andreswinckler@gmail.com>
2023-06-09 10:59:57 -0300

    CLase 3 JAVA completa

---
+++ Tecnicatura3Java/Leccion03/ModificadoresAcceso/src/paquete1/ClasedHija2.java
@@ -0,0 +1,13 @@
+
+package paquete1;
+    
+public class ClasedHija2 extends Clase2{
+    public ClasedHija2(){
+        super();
+        this.atributoDefault = "Modificacion del atributo Default";
+            System.out.println("atributoDefault " + this.atributoDefault);
+            this.metodoDefault();
+        
+    }
+    
+}
