6a3dde27c125f1375cc7fa32926e745b129f8208
Andres Winckler <andreswinckler@gmail.com>
2023-06-15 11:51:36 -0300

    Clase 8 Java completa

--- Tecnicatura3Java/Leccion05/ConversionObjetos/src/test/TestConversionObjetos.java
+++ Tecnicatura3Java/Leccion05/ConversionObjetos/src/test/TestConversionObjetos.java
@@ -7,7 +7,15 @@ public class TestConversionObjetos {
     public static void main(String[] args) {
         Empleado empleado;
         empleado = new Escritor("Juan" , 5000, TipoEscritura.CLASICO);
+        //System.out.println("empleado = " + empleado);
+        System.out.println(empleado.obtenerDetalles()); //Si queremos acceder a métodos Escritor
+        //Downcasting
+        //((Escritor)empleado).getTipoEscritura(); // Tenemos 2 opciones: esta es una
+        //escritor.gettipoEscritura();
+        
+        //Upcasting
+        //Empleado empleado2=escritor;
+        //System.out.println(empleado2.obtenerDetalles());
         
     }
 }
-// seguir Video 2 CLASE 5 java minuto 2:13.
---
+++ Tecnicatura3Java/Leccion08/ManejoEscepciones1/src/manejoescepciones1/ManejoEscepciones1.java
@@ -0,0 +1,13 @@
+
+package manejoescepciones1;
+
+public class ManejoEscepciones1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
