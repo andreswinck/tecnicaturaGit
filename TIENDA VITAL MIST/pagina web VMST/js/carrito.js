c531eb7c02dcd695246aeca8ae4871a42a65b7ee
Andres Winckler <andreswinckler@gmail.com>
2023-07-20 13:53:37 -0300

    agregando funciones al carrito

--- TIENDA VITAL MIST/pagina web VMST/js/carrito.js
+++ TIENDA VITAL MIST/pagina web VMST/js/carrito.js
@@ -1,50 +1,11 @@
-const shopcontent = document.getElementById("shopcontent");
-const verCarrito = document.getElementById("verCarrito");
-const modalContainer = document.getElementById("modal-container");
-
-let carrito = [];
-
-productos.forEach((product) => {
-    
-    // creamos un DIV donde se va a mostrar el producto
-
-    let content = document.createElement("div");
-    // le asignamos una clase para poder darle estilos
-    content.className = "card"; 
-    // le asignamos el contenido al DIV que creamos con etiquetas HTML
-    content.innerHTML = `
-    <img src="${product.img.src}">     
-    <h3>${product.nombre}</h3> 
-    <p class="price">${product.precio}$</p>`;
-    
-    //agregamos el DIV al contenedor
-    shopcontent.append(content);
-
-    //agregamos un boton para comprar
-    let comprar = document.createElement("button");
-    comprar.innerText = "Comprar";
-    comprar.className = "comprar";
-
-    //agregamos el boton al contenedor
-    content.append(comprar);
-
-    //agregamos un evento al boton comprar
-    comprar.addEventListener("click", () => {
-        //agregamos el producto al carrito
-        carrito.push({
-            id: product.id,
-            img: product.img.src,
-            nombre: product.nombre,
-            precio: product.precio,
-        });
-        console.log(carrito);
-    }
-    );
-});
-
-verCarrito.addEventListener("click", () => {
-    //mostramos el carrito
-    // console.log("aca toy!!!");
+// verCarrito.addEventListener("click", () => {
+const pintarCarrito = () => {
+    //logica del carrito
+    //esto es para que no se repita el carrito cada vez que se hace click
+    modalContainer.innerHTML = "";
+    //esto es para que muestre el carrito cada vez que se hace click
+    modalContainer.style.display = "flex";
+    //creamos el header del carrito
     const modalHeader = document.createElement("div");
     modalHeader.className = "modal-header";
     modalHeader.innerHTML = `
@@ -56,36 +17,69 @@ verCarrito.addEventListener("click", () => {
     const modalbutton = document.createElement("h1");
     modalbutton.innerText = "X";
     modalbutton.className = "modal-header-button";
-    
+    modalbutton.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+ 
     modalHeader.append(modalbutton);
-
+    
+    // agrega cada producto del carrito
     carrito.forEach((product) => {
-
     // body del carrito
-
     let carritoContent = document.createElement("div");
     carritoContent.className = "modal-content";
+    // le asignamos el contenido al DIV que creamos con etiquetas HTML
+    // imagen, nombre y precio
+
     carritoContent.innerHTML = `
+   
     <img src="${product.img}">
     <h3>${product.nombre}</h3>
-    <p class="price">${product.precio}$</p> 
+    <p class="price">${product.precio}$</p>
+    <p> Cantidad: ${product.cantidad}</p>
+    <p> Subtotal: ${product.precio * product.cantidad}$</p>
     `;
-    
     modalContainer.append(carritoContent);
 
-
+    // boton para eliminar producto
+    let eliminar = document.createElement("span");
+    // eliminar.innerText = "❎";
+    eliminar.innerHTML = `<i class="bi bi-trash borrarProducto"></i>`;
+    eliminar.className = "delete-product";
+    carritoContent.append(eliminar);
+    eliminar.addEventListener("click", eliminarProducto);
 
     });
      // footer del carrito
     // para el total acc es el acumulador y -el- es cada elemento
-    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
     const totalBuying = document.createElement("div");
     totalBuying.className = "total-content";
-    totalBuying.innerHTML = `total a pagar: ${total}$`;
+    totalBuying.innerHTML = `total a pagar: $ ${total}`;
     modalContainer.append(totalBuying);
+};
 
-   
+verCarrito.addEventListener("click", pintarCarrito);
 
+// boton para eliminar producto
+//filtrar el carrito y eliminar el producto
+const eliminarProducto = () => {
+    const foundId = carrito.find((element) => element.id);
 
+    carrito = carrito.filter((carritoId) => {
+    
+        return carritoId !== foundId;
 });
+//resta al contador del carrito
+carritoCounter();
+//muestra carrito actualizado
+pintarCarrito();
+};
+//detecta cual es la cantidad de PRODUCTOS en el carrito
+
+const carritoCounter = () => {
+    cantidadCarrito.style.display = "block";
+    cantidadCarrito.innerText = carrito.length;
+
+};
 
--- TIENDA VITAL MIST/pagina web VMST/js/products.js
+++ TIENDA VITAL MIST/pagina web VMST/js/products.js
@@ -4,7 +4,8 @@ const productos = [
         precio: 100,
         img: {
             src:"/img/frasco1.png",
-        }
+        },
+        cantidad: 1,
     },
 
     {   id: 2,
@@ -12,20 +13,23 @@ const productos = [
         precio: 200,
         img: {
             src:"/img/frasco2.png",
-        }
+        },
+        cantidad: 1,
     },
     {   id: 3,
         nombre: "Producto 3",
         precio: 300,
         img: {
             src:"/img/frasco3.png",
-        }
+        },
+        cantidad: 1,
     },
     {   id: 4,
         nombre: "Producto 4",
         precio: 400,
         img: {
             src:"/img/frasco4.png",
-        }
+        },
+        cantidad: 1,
     },
 ];
