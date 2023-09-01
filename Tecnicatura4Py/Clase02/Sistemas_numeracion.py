22740f004f149ef5c60595ab102b967ecd1ffaf2
Andres Winckler <andreswinckler@gmail.com>
2023-09-01 13:42:26 -0300

    subiendo clase 02 python 4 semestre

---
+++ Tecnicatura4Py/Clase02/Sistemas_numeracion.py
@@ -0,0 +1,32 @@
+#Profundizando en los sistemad e numeracion
+#Sistema decimal
+a = 10
+print(f"a decimal: {a}")
+#Sistema binario
+a = 0b1010
+print(f"a binario: {a}")
+#Sistema octal
+a = 0o12
+print(f"a octal: {a}")
+#Sistema hexadecimal
+a = 0xA
+print(f"a hexadecimal: {a}")
+
+#Conversiones
+a = int('23', 10)
+print(f"a decimal: {a}")
+
+a = int('1010', 2)
+print(f"a binario: {a}")
+
+a = int('12', 8)
+print(f"a octal: {a}")
+
+a = int('A', 16)
+print(f"a hexadecimal: {a}")
+
+# Base 5
+a = int('20', 5)
+print(f"a base 5: {a}")
+
+
---
+++ Tecnicatura4Py/Clase02/tipo_float.py
@@ -0,0 +1,23 @@
+# Profundizando en el tipo float
+a = 3.0
+# print(f"a: {a:.2f}")
+
+# constructor float -> puede recibir un entero o un string
+
+# a = float(10) # le paso un entero
+a = float('10') # le paso un string
+print(f"a: {a:.2f}")
+
+# notacion exponencial (positiva y negativa)
+a = 3e5
+print(f"a: {a}")
+
+a = 3e-5
+print(f"a: {a:.5f}")
+
+a = 4.0 + 5
+print(f"a: {a}")
+print(f"tipo de a: {type(a)}")
+
+
+
