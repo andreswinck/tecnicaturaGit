1621a42a6e96b27090fdb18b74ab4ab3e43e68d3
Andres Winckler <andreswinckler@gmail.com>
2023-09-05 12:13:43 -0300

    actualizando carrito JS

---
+++ Tecnicatura4JS/LECCION_1/ecommerce/cliente/js/cart.js
@@ -0,0 +1,64 @@
+const modalContainer = document.querySelector('.modal-container');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const cartBtn = document.getElementById('cart-btn');
+
+const displayCart = () => {
+    modalContainer.innerHTML = '';
+    modalContainer.style.display = 'block';
+    modalOverlay.style.display = 'block';
+        //modal Header
+    const modalHeader = document.createElement('div');
+
+    const modalClose = document.createElement('div');
+    modalClose.innerText='❎';
+    modalClose.className = 'modal-close';
+    modalHeader.append(modalClose);
+
+    modalClose.addEventListener('click', () => {
+        modalContainer.style.display = 'none';
+        modalOverlay.style.display = 'none';
+    });
+
+    
+    const modalTitle = document.createElement('div');
+    modalTitle.innerText = 'Cart';
+    modalTitle.className = 'modal-title';
+    modalHeader.append(modalTitle);
+
+    modalContainer.append(modalHeader);
+    //modal Body
+    cart.forEach(product => {
+        const modalBody = document.createElement('div');
+        modalBody.className = 'modal-body';
+        modalBody.innerHTML = `
+        <div class="product">
+            <img class="product-img" src="${product.img}"/>
+            <div class="product-info">
+                <h4>${product.productName}</h4>
+            </div>
+            <div class="quantity">
+                <span class="quantity-btn-decrese">-</span>
+                <span class="quantity-input">${product.quanty}</span>
+                <span class="quantity-btn-increase">+</span>
+            </div>
+            <div class="price">${product.price * product.quanty}$</div>
+            <div class="delete-product">❌</div>
+        </div>
+        `;
+        modalContainer.append(modalBody);
+    });
+
+    //modal Footer
+
+    const modalFooter = document.createElement('div');
+    modalFooter.className = 'modal-footer';
+    modalFooter.innerHTML = `
+        <div class="total-price">Total:</div>
+    `;
+    modalContainer.append(modalFooter);
+};
+
+cartBtn.addEventListener('click', displayCart);
+
+// clase 03 video 3 empezar
---
+++ Tecnicatura4JS/LECCION_1/ecommerce/cliente/js/products.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/LECCION_1/ecommerce/cliente/media/banana.jpg",
+    },
+    {
+        id: 2,
+        productName: "Frutilla",
+        price: 690,
+        quanty: 1,
+        img: "/LECCION_1/ecommerce/cliente/media/frutilla.jpg",
+    },
+    {
+        id: 3,
+        productName: "Kiwi",
+        price: 560,
+        quanty: 1,
+        img: "/LECCION_1/ecommerce/cliente/media/kiwi.jpg", 
+    },
+    {
+        id: 4,
+        productName: "Limon",
+        price: 320,
+        quanty: 1,
+        img: "/LECCION_1/ecommerce/cliente/media/limon.jpg",
+    },
+];
