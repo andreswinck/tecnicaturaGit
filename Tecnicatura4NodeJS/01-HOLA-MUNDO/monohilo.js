588425732ac804d6d59a76c01d4acad6fbf34e67
Andres Winckler <andreswinckler@gmail.com>
2023-09-15 13:23:12 -0300

    clase 5 Node JS

---
+++ Tecnicatura4NodeJS/01-HOLA-MUNDO/monohilo.js
@@ -0,0 +1,12 @@
+
+console.log('Hola mundo')
+var i = 0;
+setInterval(() => {
+    console.log(i);
+    i++;
+    // if (i === 5) {
+    //     console.log('forzamos error');
+    //     var a = 3 + z;
+    // }   
+}, 1000);
+console.log('Segunda instrucción');
