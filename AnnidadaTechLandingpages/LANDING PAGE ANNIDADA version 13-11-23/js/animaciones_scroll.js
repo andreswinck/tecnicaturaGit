38c77a232e65b4abf5527be7fbb12ad96842017c
Andres Winckler <andreswinckler@gmail.com>
2023-11-15 21:22:37 -0300

    NEW VERSION ANNIDADA WEB

---
+++ AnnidadaTechLandingpages/LANDING PAGE ANNIDADA version 13-11-23/js/animaciones_scroll.js
@@ -0,0 +1,74 @@
+window.sr = ScrollReveal();
+// Animacion de la barra de navegacion  
+    sr.reveal('.navbar', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px',
+    });
+
+    // sr.reveal('.la-empresa', {
+    //     duration: 3000,
+    //     origin: 'left',
+    //     distance: '-1000px',
+    //     reset: 'true',
+    //     scale: 0.2,
+    // });
+
+    sr.reveal('.servicio_izquierda', {
+        duration: 3000,
+        origin: 'right',
+        distance: '10px',
+        reset: 'true',
+        scale: 0.1,
+        mobil: 'false',
+    });
+    sr.reveal('.servicio_centro', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '10px',
+        reset: 'true',
+        scale: 0.1,
+        mobil: 'false',
+    });
+    sr.reveal('.servicio_derecha', {
+        duration: 3000,
+        origin: 'left',
+        distance: '10px',
+        reset: 'true',
+        scale: 0.1,
+        mobil: 'false',
+    });
+
+    sr.reveal('.portfolio_izquierda', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-10px',
+        reset: 'true',
+        scale: 0.1,
+        mobil: 'false',
+    });
+    sr.reveal('.portfolio_centro', {
+        duration: 3000,
+        origin: 'left',
+        distance: '10px',
+        reset: 'true',
+        scale: 0.1,
+        mobil: 'false',
+    });
+    sr.reveal('.portfolio_derecha', {
+        duration: 3000,
+        origin: 'left',
+        distance: '10px',
+        reset: 'true',
+        scale: 0.1,
+        mobil: 'false',
+    });
+
+    // sr.reveal('.contacto', {
+    //     duration: 2500,
+    //     origin: 'right',
+    //     distance: '-10px',
+    //     reset: 'true',
+    //     scale: 0.1,
+    //     mobil: 'false',
+    // });
